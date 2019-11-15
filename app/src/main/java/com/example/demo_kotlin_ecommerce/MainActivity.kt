89c2b230245c27f0e676efaa50a8beae3e4de811
IN007848 <Reshma@oppo2019>
2019-11-15 18:32:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/demo_kotlin_ecommerce/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.demo_kotlin_ecommerce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Adapter
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity()
+{
+    lateinit var viewPager: ViewPager
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //
+//https://www.youtube.com/watch?v=GdWyWWF61mw
+
+//https://www.youtube.com/watch?v=0yH1qDxqZMo
+//https://www.youtube.com/watch?v=Y8mGxxRe3k4
+
+
+    }
+
+
+}
